1f46fc6d3473c79fba42cddcd18acd778f2641b5
Ollie <ojimba01@gmail.com>
2023-06-04 02:09:16 -0400

    api update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,7 +3,7 @@ import { getDatabase, ref, get } from "firebase/database";
 import { auth } from './firebase';
 
 
-const API_BASE_URL = "http://0.0.0.0:3000";
+const API_BASE_URL = "https://lcrserver-production.up.railway.app/";
 
 
 interface Player {
