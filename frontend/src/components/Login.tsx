e2b54aed9a2cfa5ed8458234487eab8ef50381d4
Ollie <ojimba01@gmail.com>
2023-06-09 14:34:15 -0400

    Login Authentication error updates

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
 import { auth, signIn, createUser } from '../firebase';
-import { Box, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import { Box, Heading, FormControl, FormLabel, Input, Button, Text } from '@chakra-ui/react';
+import { set } from 'firebase/database';
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const loginUser = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
       await signIn(auth, email, password);
     } catch (error) {
-      console.error(error);
+      const errorCode = error.code;
+      console.log(errorCode);
+      let errorMessage = '';
+
+      // Mapping Firebase error codes to custom error messages
+      switch (errorCode) {
+        case 'auth/invalid-email':
+          errorMessage = 'Invalid email address.';
+          break;
+        case 'auth/user-disabled':
+          errorMessage = 'Your account has been disabled.';
+          break;
+        case 'auth/user-not-found':
+          errorMessage = 'User not found.';
+          break;
+        case 'auth/wrong-password':
+          errorMessage = 'Incorrect password.';
+          break;
+        case 'auth/email-already-in-use':
+          errorMessage = 'Email address already in use.';
+          break;
+        default:
+          errorMessage = 'An error occurred. Please try again.';
+          break;
+      }
+
+      setError(errorMessage);
     }
   };
 
@@ -20,22 +48,45 @@ const Login: React.FC = () => {
     try {
       await createUser(auth, email, password);
     } catch (error) {
-      console.error(error);
+      const errorCode = error.code;
+      let errorMessage = '';
+      console.log(errorCode);
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          errorMessage = 'Email address already in use.';
+          break;
+        case 'auth/invalid-email':
+          errorMessage = 'Invalid email address.';
+          break;
+        default:
+          errorMessage = 'An error occurred. Please try again.';
+          break;
+    }
+
+    setError(errorMessage);
     }
   };
 
   return (
-    <Box maxW={["sm", "md", "lg", "2xl"]} margin="0 auto" mt={20} p={6} borderWidth={1} borderRadius="md" boxShadow="md">
-      <Heading mb={6} textAlign="center">Login</Heading>
+    <Box maxW={['sm', 'md', 'lg', '2xl']} margin="0 auto" mt={20} p={6} borderWidth={1} borderRadius="md" boxShadow="md">
+      <Heading mb={6} textAlign="center">
+        Login
+      </Heading>
       <form>
         <FormControl mb={4}>
           <FormLabel>Email</FormLabel>
-          <Input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
+          <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
         </FormControl>
         <FormControl mb={4}>
           <FormLabel>Password</FormLabel>
-          <Input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
         </FormControl>
+        {error && <Text color="red.500" mb={4}>{error}</Text>}
         <Button colorScheme="blue" onClick={loginUser} mr={2} mb={4} width="100%">
           Sign In
         </Button>
@@ -45,6 +96,6 @@ const Login: React.FC = () => {
       </form>
     </Box>
   );
-}
+};
 
 export default Login;
