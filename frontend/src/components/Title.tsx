6c4375c777b9f5a7552b7ace74db7c35adbc63d5
Ollie <ojimba01@gmail.com>
2023-06-17 19:53:34 -0400

    Title link update

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,27 +1,28 @@
 import TitleIcon from "../assets/dice.svg";
-
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Link } from "@chakra-ui/react";
 
 export const TitleHeader: React.FC = () => {
   return (
     <Box
       display="flex"
       alignItems="center"
-      borderWidth={1}
-      borderRadius="md"
-      boxShadow="md"
-      p={2.5}
+      justifyContent="center"
+      flexWrap="wrap"
     >
-      <img
-        src={TitleIcon}
-        alt="Dice Icon"
-        style={{ width: "24px", height: "24px", marginRight: "0.5rem" }}
-      />
-      <Heading>
-        <a href="https://github.com/ojimba01/lcr_client">
-          <b>LCR Online (v.1.0.1)</b>
-        </a>
-      </Heading>
+      <Box mr={2}>
+        <img
+          src={TitleIcon}
+          alt="Dice Icon"
+          style={{ width: "24px", height: "24px" }}
+        />
+      </Box>
+      <Box>
+        <Heading as="h1" size="2xl" textAlign={{ base: "center", md: "left" }}>
+          <Link href="https://github.com/ojimba01/lcr_client" fontWeight="bold">
+            LCR Online (v.1.0.1)
+          </Link>
+        </Heading>
+      </Box>
     </Box>
   );
 };
